5779b6141447610ee70f40b87372e92d61eb8c39
Emdey <addeybashegs1@gmail.com>
2025-12-11 17:25:19 +0100

    Defining the Interface.
    Adding the Backend Scanner File (nmap_scanner.py), and the Logic Rules File(vuln_rules.py)

---
+++ modules/vuln_rules.py
@@ -0,0 +1,9 @@
+# modules/vuln_rules.py
+# LOGIC ENGINEER: Write your 'If-Then' rules here.
+
+def check_vulnerabilities(scan_data):
+    """
+    Input: Dictionary from the scanner.
+    Output: List of warnings (Strings).
+    """
+    return ["No rules defined yet."]
